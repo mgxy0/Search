7ae63104495fe75423a140fc2d345ee46f59cbd9
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-05-30 17:59:20 +0200

    Update search.py

--- search.py
+++ search.py
@@ -13,30 +13,10 @@ FILE_TYPES = {
     'server': ['.conf', '.log', '.sh', '.service', '.env']
 }
 
-OPEN_PROGRAMS = {
-    'image': ['open -a Preview', 'open -a Photos', 'open -a Safari'],
-    'video': ['open -a QuickTime Player', 'open -a VLC', 'open -a Safari'],
-    'document': ['open -a Preview', 'open -a Pages', 'open -a TextEdit'],
-    'audio': ['open -a Music', 'open -a QuickTime Player', 'open -a Safari'],
-    'archive': ['open -a Archive Utility', 'open -a The Unarchiver', 'open -a Safari'],
-    'server': ['open -a TextEdit', 'open -a Visual Studio Code', 'open -a Terminal']
-}
-
 SYSTEM_DIRS = ['/System', '/Library', '/bin', '/sbin', '/usr']
 
 def is_system_path(path):
     return any(path.startswith(system_dir) for system_dir in SYSTEM_DIRS)
-
-def open_file(file_path, file_type):
-    programs = OPEN_PROGRAMS.get(file_type, [])
-    for program in programs:
-        try:
-            subprocess.run(f"{program} {file_path}", shell=True, check=True)
-            break
-        except subprocess.CalledProcessError:
-            continue
-    else:
-        print(f"Could not open file {file_path} with available programs.")
 
 def filter_by_date(file_path, date_filter):
     current_time = time.time()
